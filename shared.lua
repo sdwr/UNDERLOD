03c52382edf4e9fb7820922a4f30414c541dd2b9
Rory <rorykaufmann@gmail.com>
2025-07-02 18:28:34 -0500

    commented out main_effects_canvas

--- shared.lua
+++ shared.lua
@@ -144,7 +144,7 @@ function shared_draw(draw_action)
   shadow_canvas:draw(x + 1.5*sx, y + 1.5*sy, 0, sx, sy)
   main_canvas:draw(x, y, 0, sx, sy)
   full_res_character_canvas:draw(x, y)
-  main_effects_canvas:draw(x, y, 0, sx, sy)
+  -- main_effects_canvas:draw(x, y, 0, sx, sy)
   full_res_canvas:draw(x, y)
 end
 
