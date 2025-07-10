22ae04e26eadeadf7217794e9ad692643b3ef22a
Rory <rorykaufmann@gmail.com>
2025-07-10 01:04:14 -0500

    wave tweaks

--- spawns/wave_types.lua
+++ spawns/wave_types.lua
@@ -33,6 +33,23 @@ function Wave_Types:Get_Waves(level)
     table.insert(waves, wave)
     return waves
   end
+
+  if level == 2 then
+    table.insert(wave, {'GROUP', 'seeker', 3, 'random'})
+    table.insert(wave, {'GROUP', 'singlemortar', 2, 'random'})
+    table.insert(wave, {'GROUP', 'big_goblin_archer', 1, 'random'})
+    table.insert(waves, wave)
+    return waves
+  end
+
+  if level == 3 then
+    table.insert(wave, {'GROUP', 'seeker', 5, 'random'})
+    table.insert(wave, {'GROUP', 'snakearrow', 1, 'random'})
+    table.insert(wave, {'GROUP', 'goblin_archer', 2, 'random'})
+    table.insert(wave, {'GROUP', 'cleaver', 1, 'random'})
+    table.insert(waves, wave)
+    return waves
+  end
   
   -- Step 1: Add a special enemy from the correct tier
   local special_enemy = random:table(special_enemy_by_tier[tier])
