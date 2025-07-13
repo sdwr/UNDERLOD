8787534ed18b3679932c0676085e6c550c880b92
Rory <rorykaufmann@gmail.com>
2025-07-13 15:44:47 -0500

    outline shader

--- engine/graphics/shader.lua
+++ engine/graphics/shader.lua
@@ -21,11 +21,18 @@ end
 -- Takes in a parameter and the data that corresponds to it and sends it to the shader.
 -- shader:send('displacement_map', displacement_canvas)
 function Shader:send(value, data)
-  if data:is(Canvas) then
-    self.shader:send(value, data.canvas)
-  elseif data:is(Image) then
-    self.shader:send(value, data.image)
+  -- First, check if 'data' is a table that we can call methods on.
+  if type(data) == 'table' and data.is then
+      if data:is(Canvas) then
+          self.shader:send(value, data.canvas)
+      elseif data:is(Image) then
+          self.shader:send(value, data.image)
+      else
+          -- It's some other custom object, send it as is.
+          self.shader:send(value, data)
+      end
   else
-    self.shader:send(value, data)
+      -- It's a primitive type (boolean, number, etc.). Send it directly.
+      self.shader:send(value, data)
   end
 end
